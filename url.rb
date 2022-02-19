f6cc11aef869eb9ec4622e661f25f40bdccd5915
Seo-4d696b75 <s.kaoru509@gmail.com>
2022-02-19 12:22:58 +0900

    追加：url一覧

---
+++ url.rb
@@ -0,0 +1,14 @@
+require "net/http"
+
+File.open("url.csv", "r") do |file|
+  file.each_line do |line|
+    no, _, path = line.chomp.split(",")
+    url = "https://ek1mem0.wiki.fc2.com#{path}"
+    if r = Net::HTTP.get(URI.parse(url))
+      File.open("raw/html/#{no}.html", "w") { |f| f.write(r) }
+    else
+      raise RuntimeError.new("fail to get #{url}")
+    end
+    puts url
+  end
+end
