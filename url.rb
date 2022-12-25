51db2774e9d88bc4082dd61ea4f8147d38087b1f
k_senda <k_senda@yumemi.co.jp>
2022-12-25 17:23:22 +0900

    更新：urlリスト

--- url.rb
+++ url.rb
@@ -1,14 +1,35 @@
-require "net/http"
 
-File.open("src/url.csv", "r") do |file|
-  file.each_line do |line|
-    no, _, path = line.chomp.split(",")
-    url = "https://ek1mem0.wiki.fc2.com#{path}"
-    if r = Net::HTTP.get(URI.parse(url))
-      File.open("src/html/#{no}.html", "w") { |f| f.write(r) }
-    else
-      raise RuntimeError.new("fail to get #{url}")
-    end
-    puts url
+def parse_url_item(cell)
+  no = cell[0]
+  img_path = cell[1].match(/<img.*?src="(?<src>.+?)"/)[:src]
+  m = cell[2].match(/<a.*?href="(?<href>.+?)">(?<name>.+?)<\/a>/)
+  wiki_path = m[:href]
+  name = m[:name]
+  type = cell[3]
+  attr = cell[4].match(/<img.*?alt="(?<attr>.+?)"/)[:attr]
+  color = cell[5]
+  skill_name = cell[6]
+  desc = cell[7]
+  [no, img_path, wiki_path, name, type, attr, color, skill_name, desc]
+end
+
+def parse_url_list(str)
+  m = str.match(/<table.*?>(?<body>.+?)<\/table>/m)
+  body = m[:body]
+  rows = body.scan(/<tr>.+?<\/tr>/m)
+  rows.map do |row|
+    row.scan(/<td.*?>(.+?)<\/td>/m).map{|e| e[0]}
+  end.select do |e|
+    e.length > 0
+  end.map do |e|
+    parse_url_item(e).join(",")
   end
 end
+
+result = Dir.glob("src/*.html").map do |path|
+  File.open(path).read
+end.map do |str|
+  parse_url_list(str)
+end.flatten.join("\n")
+
+File.open("src/url.csv", "w:utf-8").write(result)
